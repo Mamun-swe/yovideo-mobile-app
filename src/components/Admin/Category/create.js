2c4df59cd5253830cef0c38fb7d9e96f4b678008
Mamun-swe <mhmamun166009@gmail.com>
2020-06-06 03:06:13 +0600

    okk

--- src/components/Admin/Category/create.js
+++ src/components/Admin/Category/create.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
+import cat from '../../../assets/static/play.png';
 
 class create extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            cat_name: "",
+            cat_image: null,
+            cat_name_err: "",
+            cat_image_err: "",
+            previewURL: null
+        }
     }
+
+
+    onChangeCatName = (event) => {
+        this.setState({
+            cat_name: event.target.value
+        })
+    }
+
+    onChangeFileHandeller = (event) => {
+        var file = event.target.files[0];
+        this.setState({
+            cat_image: file,
+            previewURL: URL.createObjectURL(event.target.files[0])
+        })
+        console.log(file.naturalHeight)
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
+
+
     render() {
+        const previewFile = this.state.previewURL;
         return (
-            <div className="category-create">
-                <h1>category create</h1>
+            <div className="admin-category-create">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 py-2">
+                            <h5 className="mb-0">Create new category</h5>
+                        </div>
+                        <div className="col-12 pt-3 pb-2">
+                            <form onSubmit={this.handleSubmit}>
+
+                                {/* Category Name */}
+                                <div className="form-group mb-3">
+                                    <label>Category Name</label>
+                                    <input type="text" onChange={this.onChangeCatName} className="form-control rounded-0 shadow-none" placeholder="Enter category name" />
+                                </div>
+
+                                {/* Category Poster */}
+                                <div className="form-group mb-3">
+                                    <label>Category poster</label>
+                                    <br />
+                                    <input type="file" id="file" name="file" className="inputfile" onChange={this.onChangeFileHandeller} />
+                                </div>
+
+                                <div className="form-group mb-3">
+                                    {
+                                        previewFile ? (
+                                            <img src={this.state.previewURL} className="img-fluid" alt="Preview" />
+                                        ) : (
+                                                <img src={cat} className="img-fluid" alt="category" />
+                                            )
+                                    }
+                                </div>
+
+                                <button type="submit" className="btn btn-block btn-dark rounded-0 shadow-none text-white">Create Now</button>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
